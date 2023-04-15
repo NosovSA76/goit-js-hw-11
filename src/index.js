092c2f08a258f37257170a51a59ade86ff02b970
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-04-15 14:55:49 +0300

    15/04

---
+++ src/index.js
@@ -0,0 +1,68 @@
+// import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import FotoService from './js/fetchFoto';
+// import { murkupGallery } from './js/markupGallery';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery')
+}
+
+const fotoService = new FotoService();
+
+console.log(fotoService);
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  clearGallery();
+  fotoService.query = e.currentTarget.elements.searchQuery.value;
+  fotoService.resetPage();
+  fotoService.fetchFoto().then(hits => murkupGallery(hits));
+}
+
+const murkupGallery = (hits) => {
+  const imagesMarkup = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+    `<div class="photo-card">
+   <a href="${largeImageURL}" target="_blank" class="gallery-item">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+</a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes} </b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views} </b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`
+  ).join('');
+
+  refs.gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+  var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  lightbox.refresh()
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = "";
+}
+
+window.addEventListener('scroll', debounce(() => {
+  const documentRect = document.documentElement.getBoundingClientRect()
+  console.log(documentRect.bottom, documentRect.height)
+  const mid = document.documentElement.clientHeight
+  console.log(mid)
+  if (documentRect.bottom < mid + 500) {
+  fotoService.fetchFoto().then(hits => murkupGallery(hits));
+  }
+}, 200))
---
+++ src/js/fetchFoto.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export default class FotoService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+fetchFoto() {
+    const url = `https://pixabay.com/api/?key=35176163-4e9ce6048ecabe9e72c7fef0e&q=${this.searchQuery}&image_type=photo&page=${this.page}&per_page=40&min_width=250&min_height=150&orientation=horizontal`;
+
+    return axios.get(url)
+      .then(response => {
+        this.page += 1;
+        return response.data.hits;
+      })
+      .catch(error => console.log(error));
+}
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
