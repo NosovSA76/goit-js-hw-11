6ae638200d72aa0adc781e4800fc1abbf90a73ec
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-05-28 10:33:19 +0300

    28.05

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-export { refs }
-
-//import './css/styles.css';
+// import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import SimpleLightbox from "simplelightbox";
@@ -8,62 +6,59 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import FotoService from './js/fetchFoto';
 import { refs } from './js/refs.js'
 import { murkupGallery } from './js/markupGallery';
-// import { addMoreFoto } from './js/addMoreFoto';
-
 
 var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-
 const fotoService = new FotoService();
-
 
 refs.searchForm.addEventListener('submit', onSearch);
 
 async function onSearch(e) {
   e.preventDefault();
   clearGallery();
-  fotoService.query = e.currentTarget.elements.searchQuery.value.trim().toLowerCase();
   fotoService.resetPage();
-
-
-  try {
-    const { hits, total } = await fotoService.fetchFoto();
-    const murkup = murkupGallery(hits);
-    refs.gallery.insertAdjacentHTML('beforeend', murkup);
-    lightbox.refresh();
-    if (total > 0) {
-      Notify.success(`Hooray! We found ${total} images. But we can show only 520`);
-    }
-    else {
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-
-    }
-  } catch (error) {
-    Notify.failure('Something went wrong!');
-  }
+  fotoService.query = e.currentTarget.elements.searchQuery.value.trim().toLowerCase();
+  console.log(fotoService.query);
+  addMoreFoto();
 }
-
 
 function clearGallery() {
   refs.gallery.innerHTML = "";
 }
 
-
-const debouncedAddMoreFoto = debounce(addMoreFoto, 500);
-
-async function addMoreFoto() {
-  const documentRect = document.documentElement.getBoundingClientRect();
-  const mid = document.documentElement.clientHeight;
-
-  if (documentRect.bottom < mid + 500) {
-    try {
-     const { hits } = await fotoService.fetchFoto();
-    const murkup = murkupGallery(hits);
-    refs.gallery.insertAdjacentHTML('beforeend', murkup);
+const addMoreFoto = async () => {
+  try {
+    const { hits, total } = await fotoService.fetchFoto();
+    const markup = murkupGallery(hits);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    console.log(fotoService.page);
     lightbox.refresh();
-    } catch (error) {
-       Notify.failure('We are sorry, but you have reached the end of search results.');
-    }
-  }
-}
 
-window.addEventListener('scroll', debouncedAddMoreFoto);
+    const maxRequests = Math.ceil(total / fotoService.perPage);
+    if (fotoService.page >= maxRequests) {
+      console.loge(maxRequests)
+      Notify.failure("We're sorry, but you've reached the end of search results.");
+      observer.disconnect();
+      return;
+    }
+    observer.observe(refs.gallery.lastElementChild);
+  } catch (error) {
+    Notify.failure('Something went wrong!');
+    observer.disconnect();
+  }
+};
+
+// Створення спостерігача
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+        addMoreFoto();
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+// Початкове спостереження
+observer.observe(refs.gallery.lastElementChild);
